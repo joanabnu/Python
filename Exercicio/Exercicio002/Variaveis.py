d4cacec68b3693b26e28ae26867c997158bfc2f6
Joana Maria <joanamariasa2611@gmail.com>
2023-01-15 20:46:56 -0300

    Variaveis

---
+++ Exercicio/Exercicio002/Variaveis.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome ? ')
+idade = input('Qual é a sua idade ? ')
+peso = input('qual é seu peso ? ')
+print('Seu nome é : {} sua idade é : {} seu peso é : {}'.format(nome,idade,peso) )
