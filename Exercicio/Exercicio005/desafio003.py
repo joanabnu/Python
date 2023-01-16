381e7f6297043e592ea61724dfe776e641a0869c
Joana Maria <joanamariasa2611@gmail.com>
2023-01-15 21:42:26 -0300

    Desafio003

---
+++ Exercicio/Exercicio005/desafio003.py
@@ -0,0 +1,4 @@
+n1=int(input('Digite um número : '))
+n2=int(input('Digite outro número : '))
+soma = n1+n2
+print('O número que voce digitou são : {} e {}. A soma do numero é : {}'.format(n1,n2,soma))
