a3528ca8821edac3502b8b4e5eb8f6c044765b3b
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-01-15 14:12:01 -0300

    Desafio 04

---
+++ CursoemvideoPython/Desafio04.py
@@ -0,0 +1,8 @@
+a = input('Digite algo ')
+print('O tipo primitivo desse valor é ',type(a))
+print('So tem espaço :', a.isspace())
+print('É um numero : ', a.isnumeric())
+print('É alfabetico : ', a.isalpha())
+print('É alfanumerico :',a.isalnum())
+print('Esta em maiuscula : ',a.islower())
+print('Esta capitalizada : ', a.istitle())
