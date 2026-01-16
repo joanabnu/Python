763ed397bdf19ccf1c9d9d4c7eecfe7f1a796c4b
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-01-16 14:28:58 -0300

    Exercicio 06

---
+++ CursoemvideoPython/Exercicio06.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero : '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale:  {}'.format(n,d))
+print('O tripla de {} vale : {}. \nA raiz quadrado de {} é igual a {} '.format(n,t,n,r))
