11839955711803a376813921022b11d68c8063fc
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-01-16 14:44:31 -0300

    Exercicio 06
    O dobro, o triplo  e raiz quadrada

--- CursoemvideoPython/Exercicio06.py
+++ CursoemvideoPython/Exercicio06.py
@@ -2,4 +2,4 @@ n = int(input('Digite um numero : '))
 
 t = n * 3
 r = n ** (1/2)
-print('O dobro de {} vale: {} \nO tripla de {} vale : {}. \nA raiz quadrado de {} é igual a {} '.format(n,(n*2),n,(n*3),n,(n**(1/2))))
+print('O dobro de {} vale: {} \nO triplo de {} vale : {}. \nA raiz quadrado de {} é igual a {} '.format(n,(n*2),n,(n*3),n,pow(n,81(1/2))))
