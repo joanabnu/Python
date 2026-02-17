8e1cf19d3f96bd8dcd61fc5075cc7c7223560934
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-17 14:43:04 -0300

    o quanto de tinta é necessario

--- CursoemvideoPython/Exercicio011.py
+++ CursoemvideoPython/Exercicio011.py
@@ -2,3 +2,5 @@ larg = float(input('Largura da parede : '))
 alt = float(input('Altura da parede : '))
 area = larg * alt
 print('Sua parede tem dimensao de {} X {} e sua area é de  {}m'.format(larg, alt,area))
+tinta = area / 2
+print('Para pintar essa parede voce precisa de {} de tinta '.format(tinta))
