fbcdefd4c9cae515bfa04f1bc9f4665766361cc6
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-17 14:41:02 -0300

    Exercicio de 11
    leia a largura e altura da parede e calcule a area e a quantidade de tinta necessaria.

---
+++ CursoemvideoPython/Exercicio011.py
@@ -0,0 +1,4 @@
+larg = float(input('Largura da parede : '))
+alt = float(input('Altura da parede : '))
+area = larg * alt
+print('Sua parede tem dimensao de {} X {} e sua area é de  {}m'.format(larg, alt,area))
