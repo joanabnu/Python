b77c22d8469b337460f3a6bd3c46541745c0e6fb
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-24 17:03:36 -0300

    Aula 09
    As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().

---
+++ CursoemvideoPython/Aula09.py
@@ -0,0 +1,11 @@
+frase = 'Curso em video Python'
+print(frase[1:15:2])
+print(frase[::2])
+print(frase.count('o'))
+print(frase.upper())
+print(frase.strip())
+print(frase.lower())
+print(frase.find('video'))
+frase1 = frase.replace('Puthon','Android')
+
+print(frase1)
