d56437b9ab2b9027fa885dc57fa39895e69ee55a
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-01 15:32:42 -0300

    Exercicio 22

--- Exercicio022.py
+++ Exercicio022.py
@@ -1,2 +1,6 @@
 nome = str(input('Digite o seu nome completo: ')).strip()
 print('O seu nome é : {}\nO seu nome com letra maiuculas : {}\nO seu nome com letras minuculas {}\nQuantas letras tem o seu nome : {}\nSeu primeiro nome tem {}'.format(nome,nome.upper(),nome.lower(),len(nome)-nome.count(' '),nome.find(' ')))
+
+print('Utilizando split()')
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letra'.format(separa[0], len(separa[0])))
