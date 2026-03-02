a4417c2afe05876badd5df4fec83c86c83fc4693
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-02 15:03:21 -0300

    Exercicio de cores

--- Aula11.py
+++ Aula11.py
@@ -4,7 +4,8 @@
 # text 30 =  branco - 31 = vermelho - 32 = verde - 34 = azul - 35 = lilas - 36 =  azul claro - 37 = cinza
 #  back 40 = branco - 41 = vermelho - 42 =  verde - 43 = amarelho - 44 = azul escuro -  45 = lilas - 46 = azul claro - 47 - cinza
 
-# print('\033[31mOlá mundo')
-#  print('\033[1;31;43mOlá a todos')
-#  print('--'*30)
+print('\033[31mOlá mundo')
+print('\033[1;31;43mOlá a todos')
+print('--'*30)
 print('\033[4;30;45mOlá de novo!\033[m')
+print('\033[32mahhhhhhh\033m')
