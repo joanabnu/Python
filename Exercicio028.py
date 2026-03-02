da91fa798af15b38fd2e7abfd8fd9d13178fdfcf
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-01 22:25:10 -0300

    sleep

--- Exercicio028.py
+++ Exercicio028.py
@@ -1,9 +1,12 @@
 from random import randint
+from time import sleep
 computador = randint(0,5)
 print('-=-' * 20)
 print('Vou pensar em um numero entre 0 e 5. Tente adivinhar ')
 print('-=-' * 20)
 jogador = int(input('Em que numero eu pensei? '))
+print('Processando')
+sleep(2)
 if jogador == computador:
     print('Parabens! Voce conseguiu me vencer!')
 else:
