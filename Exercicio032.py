97e5a735d9501202eba0e30ba43c0ebc5ade0912
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-01 23:40:49 -0300

    Exercicio32

--- Exercicio032.py
+++ Exercicio032.py
@@ -1,4 +1,7 @@
-ano = int(input('Que ano quer analisar : '))
+from datetime import date
+ano = int(input('Que ano quer analisar ? COloque 0 para analisar ano atual : '))
+if ano == 0:
+    ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é BISSEXTO'.format(ano))
 else: 
