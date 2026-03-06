44361a66b783790ae95972e26eb0795f424c7019
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-06 19:04:53 -0300

    Exercicio 037

--- CursoemvideoPython/Python 3 Mundo 2/Exercicio037.py
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio037.py
@@ -3,10 +3,10 @@ escolha = int(input('\no Informe a base de conversão :  '))
 num = int(input('Escolhar um numero : '))
 
 if escolha == 1:
-    print('Numero : {} Convertido para Binario : {}'.format(num,bin(escolha)))
+    print('Numero : {} Convertido para Binario : {}'.format(num,bin(escolha)[2:]))
 elif escolha == 2:
-    print('Numero : {} Convertido para Octal : {}'.format(num,oct(num)))
+    print('Numero : {} Convertido para Octal : {}'.format(num,oct(num)[2:]))
 elif escolha == 3:
-    print('Numero : {} Convertido para Hexadecimal : {}'.format(num,hex(num)))
+    print('Numero : {} Convertido para Hexadecimal : {}'.format(num,hex(num)[2:]))
 else:
     print('Numero Errado')
