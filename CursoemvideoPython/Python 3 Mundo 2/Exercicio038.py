ead74c7284553a16094e54cd46cc6f68492ed45a
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-08 14:11:51 -0300

    Exercicio 0 38

---
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio038.py
@@ -0,0 +1,8 @@
+PrimeiroNumero = int(input('Primeiro Numero : '))
+SegundaNumero = int(input('Segunda Numero : '))
+if PrimeiroNumero > SegundaNumero:
+    print('O primeiro numero é Maior')
+elif  SegundaNumero > PrimeiroNumero:
+    print('Segundo numero é Maior')
+else:
+    print('Os dois valores são iguais')
