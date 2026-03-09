2a9cdd0c9daf3f9c70bd62643bf17613686a792f
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-09 14:32:20 -0300

    Aula 13

---
+++ CursoemvideoPython/Python 3 Mundo 2/Aula013.py
@@ -0,0 +1,16 @@
+# for c in range(0,6):
+#     print(c)
+# print('fim')
+
+# n = int(input('Digite um numero : '))
+
+# for c in range(0,n):
+#     print(c)
+# print('fim')
+
+i = int(input('Inicio : '))
+f = int(input('FIM : '))
+p = int(input('Passo :'))
+for c in range(i,f+1,p):
+    print(c)
+print('Fim')
