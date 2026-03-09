ec539e5ccefd914c4c47746de3fa7e7c2a0751f4
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-09 15:19:09 -0300

    Exercicio 049

--- CursoemvideoPython/Python 3 Mundo 2/Exercicio049.py
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio049.py
@@ -1,5 +1,6 @@
 tabuada = int(input('Qual numero tabuada : '))
 mult = 0
+print('{:=^40}'.format('TABUADA'))
 for c in range(0,11):
     mult = tabuada * c
     print('{} X {} = {}'.format(c,tabuada,mult))
