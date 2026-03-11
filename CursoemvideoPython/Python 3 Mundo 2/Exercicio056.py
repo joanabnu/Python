5d3a4cabb7c5d19407a17871edc549f2cdcc44f4
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-11 16:46:41 -0300

    Exercicio 56

---
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio056.py
@@ -0,0 +1,25 @@
+mediaIdade = 0
+maiorIdade = 0
+totalF = 0
+maioridadehomen = 0
+nomeVelho = ''
+for c in range(1,4):
+    print('-------- {}° Pessoa --------'.format(c,))
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('[M/F\nSexo:')).upper()
+    mediaIdade = (idade +idade ) / 4
+    if c == 1 and sexo in 'M':
+        maioridadehomen = idade
+        nomeVelho = nome
+    if sexo in 'M' and idade > maioridadehomen:
+        maioridadehomen = idade
+        nomeVelho = nome
+    if sexo == 'F':
+        totalF += 1
+
+print('Media de idade é {} \nTotal de mulheres : {}'.format(mediaIdade,totalF))
+print('O homen mais velho tem {} anos e se chama {}.'.format(maioridadehomen,nomeVelho))
+
+  
+
