c23a797f37848e1d2d0e260c810e7190cdf329a2
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-12 16:51:53 -0300

    Exercicio 59

--- CursoemvideoPython/Python 3 Mundo 2/Exercicio059.py
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio059.py
@@ -1,27 +1,31 @@
+print('{:=^40}'.format('Informando Valores'))
 n1 = int(input('Primeiro valor : '))
 n2 = int(input('Segundo valor : '))
+
 opcao = 0
 soma = 0
 mult = 0
 maior = 0
 while opcao != 5:
+    print('{:=^40}'.format('Menu'))
     print('[1] Somar \n[2] Multiplicar \n[3] Maior \n[4] Nova numeros \n[5] Sair do pragramar')
+    print('{:=^40}'.format('Informe a Opção'))
     opcao = int(input('Qual é sua opção : '))
     if opcao == 1:
          soma = n1 + n2
-         print('\033[35mA soma {} entre {} e {} '.format(soma,n1,n2))
+         print('\033[1;35;46mA soma {} entre {} e {} '.format(soma,n1,n2))
     elif opcao == 2:
          mult = n1 * n2
-         print('\033[32mO valor da multiplicação : {}'.format(mult))
+         print('\033[1;36;43mO valor da multiplicação : {}'.format(mult))
     elif opcao == 3:
          if n1  > n2:
             maior = n1
-            print('\033[34mO maior valor é {}'.format(n1))
+            print('\033[1;32;46mO maior valor é {}'.format(n1))
          else:
             maior = n2
             print('O maior valor é {}'.format(n2))
     elif opcao == 4:
-         print('Informe novos numeros')
+         print('{:=^40}'.format('Digite novamente'))
          n1 = int(input('Primeiro valor : '))
          n2 = int(input('Segundo valor : '))
          
