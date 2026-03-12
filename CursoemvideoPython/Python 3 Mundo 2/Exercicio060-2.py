a097beba827dc1d3b733f58283930e596bb869c9
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-12 17:09:50 -0300

    Exercicio060

---
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio060-2.py
@@ -0,0 +1,12 @@
+n = int(input('Digite um numero para calcular seu fatorial : '))
+c = n
+f = 1
+print('Calculando {}! \n'.format(n),end='')
+while c > 0:
+    print('{} '.format(c),end='')
+    print(' x ' if c >1 else '=', end='')
+  
+    f *= c
+    c -=1
+
+print(' {}'.format(f))
