56db54c1daad1f7ff1415d3ad0e7ed7f1828cb5b
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-12 15:34:06 -0300

    Exercicio 058

--- CursoemvideoPython/Python 3 Mundo 2/Exercicio058.py
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio058.py
@@ -1,8 +1,10 @@
 from random import randint
 computador = randint(0,10)
+tentativas = 0
 print('O computador pensou em numero entre 0 e 10\nVoce conseguir adivinha !! ')
 palpite = int(input('Qual é o palpite : '))
 
 while computador != palpite:
     palpite = int(input('Tente novamente \nQual é o palpite : '))
-print('Paraben voce acertou!! \nNumero do compuatador : {}'.format(computador))
+    tentativas +=1
+print('Paraben voce acertou!! \nApos a quantidade de tentativas : {} \nNumero do compuatador : {}'.format(tentativas,computador))
