e221e6f6be8ee2e14808ad8614fd0db73b04fee9
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-13 00:13:51 -0300

    Exercicio 70

--- CursoemvideoPython/Python 3 Mundo 2/Exercicio070.py
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio070.py
@@ -1,14 +1,24 @@
-quantValor = soma = 0
+quantValor = menor = soma = barato = cont = 0
 while True:
    print('=='*20)
    print('LOJA SUPER BARATÃO')
    print('=='*20)
    produto = str(input('Nome do produto : '))
    valor = float(input('Preço : '))
+   cont+=1
+   if cont == 1:
+      menor = valor
+   else:
+      if valor < menor:
+         menor = valor
+         barato = produto
    if valor >= 1000:
-      quantValor +=1
+      quantValor +=1     
    soma += valor
    prosseguir = str(input('S/N \nQuer prosseguir : ')).strip().upper()[0]
    if prosseguir != 'S':
       break
-print(f'Total da comprar : {soma} \nProduto custando mais R$1.000 \n Quantidade : {quantValor}')
+print('=='*20)
+print('fim')
+print('=='*20)
+print(f'Total da comprar : {soma} \nProduto custando mais R$1.000 \nQuantidade : {quantValor} \nO produto mais barata \nNome : {barato} Valor : {menor}')
