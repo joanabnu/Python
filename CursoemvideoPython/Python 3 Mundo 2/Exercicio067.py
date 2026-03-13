d38542484a12ab3aae8c74845a607627410a44a4
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-12 22:52:16 -0300

    Exercicio067

---
+++ CursoemvideoPython/Python 3 Mundo 2/Exercicio067.py
@@ -0,0 +1,16 @@
+n  = tabu= 0
+
+
+while True:
+    print('=='*15)
+    n = int(input('Quer ver a tabuada de qual valor : '))
+    print('=='*15)
+    if n < 0:
+        break
+    cont = 0
+    while cont <=10:
+
+        print('{} X {} = {}'.format(n,cont,n*cont))
+        cont+=1
+  
+print('Tabuada encerrada, Volte sempre')
