3bb6a76372bebd89827973b83f0ce39fef5c4052
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-17 16:00:27 -0300

    Aula16

--- CursoemvideoPython/Python3 mundo 3/aula16.py
+++ CursoemvideoPython/Python3 mundo 3/aula16.py
@@ -6,3 +6,10 @@ print(lanche[-2:])
 for comida in lanche:
     print(f'Eu vou comer {comida}')
 print('Comi pra caramba')
+
+for cont in range (0,len(lanche)):
+    print(f'{lanche[cont]} na posicao {cont}')
+print('Comi pra caramba')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posicao {pos}')
