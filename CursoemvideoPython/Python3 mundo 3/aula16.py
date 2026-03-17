f3dbaf62ed0d9a9d05ddc3dcec2b69480fb67b05
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-17 15:50:55 -0300

    Aula16

--- CursoemvideoPython/Python3 mundo 3/aula16.py
+++ CursoemvideoPython/Python3 mundo 3/aula16.py
@@ -1,3 +1,8 @@
 lanche = ('Hamburguer','Suco','Pizza', 'Pudim')
+# Tuplas são imutaveis
 print(lanche[1])
+print(lanche[-2:])
 
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba')
