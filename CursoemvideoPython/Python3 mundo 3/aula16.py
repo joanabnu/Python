9f389968f6bafaa0febd128e678a1777ba5c6144
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-17 16:06:54 -0300

    Aula16

--- CursoemvideoPython/Python3 mundo 3/aula16.py
+++ CursoemvideoPython/Python3 mundo 3/aula16.py
@@ -6,10 +6,17 @@ print(lanche[-2:])
 for comida in lanche:
     print(f'Eu vou comer {comida}')
 print('Comi pra caramba')
-
+print('=='* 20)
 for cont in range (0,len(lanche)):
     print(f'{lanche[cont]} na posicao {cont}')
 print('Comi pra caramba')
-
+print('==' * 20)
 for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posicao {pos}')
+
+print('=='*20)
+a = (2,5,4)
+b = (5,8,1,2)
+c = b + a
+print(c.count(5))
+print(c.index(5,1))
