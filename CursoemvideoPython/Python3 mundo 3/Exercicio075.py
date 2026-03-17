7d68e7d156c6267a8f9edeabe60206da4ae32eeb
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-17 18:35:27 -0300

    Exercicio075

--- CursoemvideoPython/Python3 mundo 3/Exercicio075.py
+++ CursoemvideoPython/Python3 mundo 3/Exercicio075.py
@@ -4,4 +4,10 @@ num = (int(input('Digite um numero : ')),
            int(input('Digite um numero : ')))
 print(f'Voce digitou os valores {num}')
 print(f'O valor 9 apareceu {num.count(9)} vezes')
-print(f'O valor 3 apareceu na {num.index(3)+1} posição ')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1} posição ')
+else:
+    print('O valor tres nao foi digitado em nenhuma posicao')
+for n in num:
+    if n% 2 == 0:
+        print(n,end=' ')
