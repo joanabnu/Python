a28c6fc1e6e85756f643c0525dd00a60942a9b36
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-17 16:32:51 -0300

    Exercicio 072

---
+++ CursoemvideoPython/Python3 mundo 3/Exercicio072.py
@@ -0,0 +1,9 @@
+cont = ('Zero', 'um','dois','tres', 'quatro','cinco','seis','sete','oito','nove','dez','doze','treze','catorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+
+while True:
+    num = int(input('Digite um numero entre 0 e 20 :' 
+    ''))
+    if 0 <= num <=20:
+        break
+    print('Tente novamente',end='')
+print(f'Voce digitou o numero {cont[num]}')
