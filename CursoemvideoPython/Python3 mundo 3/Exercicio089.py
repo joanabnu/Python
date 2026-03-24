7c9775f280e6e0eeb4564f3651094ce3eaea6fae
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-24 16:14:49 -0300

    Exercicio089

--- CursoemvideoPython/Python3 mundo 3/Exercicio089.py
+++ CursoemvideoPython/Python3 mundo 3/Exercicio089.py
@@ -10,4 +10,17 @@ while True :
     if contin == 'N':
         break
 print('='*30)
-print(ficha)
+print(f'{"NO.":^4}{"Nome":<10}{"Media":>80}')
+print('='*26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4} {a[0]:<10}{a[2]:>8.1f}')
+
+while True:
+    print('-'* 35)
+    opc = int(input('Mostrar notas de qual aluno ? (999 interrompe)'))
+    if opc == 999:
+        print('Finalizando ')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print(' --- Volte sempre!! --- ')
