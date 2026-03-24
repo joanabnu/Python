317e4c5f556936734830cc2e5b12c9ae20fd7018
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-24 13:50:35 -0300

    Exercicio084

---
+++ CursoemvideoPython/Python3 mundo 3/Exercicio084.py
@@ -0,0 +1,31 @@
+pessoa = list()
+dado = list()
+pessoaPesada = list()
+pessoaLeve = list()
+QuantPessoa = 1
+quantPessoaPessada = 0
+quantPessoaleve = 0
+while True:
+    dado.append(str(input('Informe o seu nome : ')))
+    dado.append(int(input('Informe o seu peso : ')))
+    pessoa.append(dado[:])
+    dado.clear()
+    QuantPessoa +=1
+    contin = str(input('Sim / Não \nInforme se gostaria de continua : ')).upper().strip()
+ 
+    if contin == 'N':
+        break
+print(pessoa)
+print(f'Quantidade : {QuantPessoa}')
+
+for p in pessoa:
+    if p[1] >= 100:
+        pessoaPesada.append(pessoa)
+        print(f'A pessoa mais pessada {p[0]} \nQuantidade de pessoa acima do peso {p[1]}')
+        quantPessoaPessada += 1
+    else:
+        pessoaLeve.append(pessoa)
+        print(f'A pessoa mais leve é : {p[0]} \nQuantidade de pessoa acima do peso {p[1]} ')
+        quantPessoaleve += 1
+print(f'Quantidade de pessoa pessada : {quantPessoaPessada}')
+print(f'Quantidade de pessoa leve : {quantPessoaleve}')
