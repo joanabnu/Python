ad607dcdd8d153677842fe6b6dd51f35a20faa61
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-24 16:58:14 -0300

    Aula19

---
+++ CursoemvideoPython/Python3 mundo 3/Aula19.py
@@ -0,0 +1,16 @@
+pessoa = {'nome': 'Gustavo', 'sexo': 'M', 'Idade': 22}
+del pessoa['sexo']
+pessoa['nome'] = 'Leandro'
+print( pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+for k, v in pessoa.items():
+    print(f'{k} = {v}')
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro ','sigla': 'RJ' }
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[1]['uf'])
