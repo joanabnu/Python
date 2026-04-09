cba9370a6c5b0823460d33000bdee01d62bb6493
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-04-09 09:48:53 -0300

    Aula 20

---
+++ CursoemvideoPython/Python3 mundo 3/Aula020.py
@@ -0,0 +1,20 @@
+# def lin():
+#     print('='*30)
+
+
+def mensagem(msg):
+    print(msg)
+
+
+mensagem(' => Aprendendo Python <= ')
+
+
+def  soma(a,b):
+   s = a + b
+   print(f'A soma :  {s}')
+
+
+soma(4,5)
+soma(8,9)
+soma(2,1)
+
