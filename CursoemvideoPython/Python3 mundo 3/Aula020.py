3fb7106043b08b3e482cb2abf3d50af3070aa4d8
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-04-09 10:07:58 -0300

    Aula020

--- CursoemvideoPython/Python3 mundo 3/Aula020.py
+++ CursoemvideoPython/Python3 mundo 3/Aula020.py
@@ -18,4 +18,13 @@ def  soma(a,b):
 soma(a=4,b=5)
 soma(b=8,a=9)
 soma(a=2,b=1)
+print('='*30)
+def contador(* num):
+    a = len(num)
+    print(f' são ao todo \n {a}° : {num}',end=' ')
+    
+
+contador(2,7,9)
+contador(4,9,6,9)
+contador(2,5,4,9,8)
 
