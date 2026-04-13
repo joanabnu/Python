7bbd99b425ca6eea89bd5adf1283a659782fcb9e
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-04-13 10:18:14 -0300

    Exercicio096

---
+++ CursoemvideoPython/Python3 mundo 3/Exercicio096.py
@@ -0,0 +1,10 @@
+def area(largura, comprimento):
+    mul = largura * comprimento
+    print(f'Largura : {largura}\n Comprimento : {comprimento}\n Total : {mul}')
+
+
+# Programa principal
+print('Controle de Terrenos ')
+l = float(input('Largura : '))
+c = float(input('Comprimento : '))
+area(l,c)
