a0e36e3fa34209f139415dd250a7789d92395eb2
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-04-20 16:40:25 -0300

    Exercicio0100

---
+++ CursoemvideoPython/Python3 mundo 3/Exercicio0100.py
@@ -0,0 +1,23 @@
+from random import randint
+from time import sleep
+
+def sorteia(lista):
+    for cont in range(0,5):
+        n = randint(1,10)
+        lista.append(n)
+        print(f'{n} ', end=' ', flush=True)
+        sleep(0.3)
+    print('Pronto')
+
+
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos a {soma}')
+numeros = list()
+sorteia(numeros)
+print(numeros)
+somaPar(numeros)
