47e3e4c7072de2ed1a3ddb5f308be2cf5181e0ad
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-06-09 12:39:25 -0300

    Modularização

---
+++ CursoemvideoPython/Python3 mundo 3/newprojets/Modulos/numeros.py
@@ -0,0 +1,4 @@
+from uteis import cal
+num = int(input('Digite um numero : '))
+fat = cal.fatorial(num)
+print(f'O fatorial de {num} e {fat} \nNumero : {num} O dobro :  {cal.dobro(num)} \nNumero : {num} O triplo : {cal.tripla(num)}')
